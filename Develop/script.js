1af9fe95d7b8b0be525f255a82215656d309f48f
Anna <am-rabota@mail.ru>
2021-08-19 20:58:37 -0700

    password generator randomizer

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(pwdLength, useUppercase, useLowercase, useNumbers, useSpecial) {
+  var randLowerChar = 'abcdefghijklmnopqrstuvwxyz';
+  var randUpChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var randDigit = '0123456789';
+  var randSpecial = '!"#$%&\'()*+,-./:;<=>?@\\[]^_`{|}~';
+  password = "";
+
+  for(var i = 0; i < pwdLength; i++) {
+    password += (randLowerChar + randUpChar + randDigit + randSpecial).charAt(Math.floor(Math.random() * (randLowerChar + randUpChar + randDigit + randSpecial).length));
+  }
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(10, true, true, false, false);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
