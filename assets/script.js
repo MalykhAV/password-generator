e40fa6448322d020f0f6e0ec63bc654f97f09e9f
Anna <am-rabota@mail.ru>
2021-12-20 16:20:02 -0800

    checkboxes added

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let inputs = document.getElementsByTagName("input");
+let getLength = inputs[0];
 
 function generatePassword(pwdLength, useUppercase, useLowercase, useNumbers, useSpecial) {
-  var randLowerCase = 'abcdefghijklmnopqrstuvwxyz';
-  var randUpCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var randDigit = '0123456789';
-  var randSpecial = '!"#$%&\'()*+,-./:;<=>?@\\[]^_`{|}~';
+  const randLowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  const randUpCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const randDigit = '0123456789';
+  const randSpecial = '!"#$%&\'()*+,-./:;<=>?@\\[]^_`{|}~';
   password = "";
 
-  var randomChars = "";
+  let randomChars = "";
   if (useUppercase) {
     randomChars += randUpCase;
   }
@@ -22,48 +23,50 @@ function generatePassword(pwdLength, useUppercase, useLowercase, useNumbers, use
     randomChars += randSpecial; 
   }
 
-  for(var i = 0; i < pwdLength; i++) {
+  for(let i = 0; i < pwdLength; i++) {
     password += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
   }
   return password;
 }
 
-
 // Write password to the #password input
 function writePassword() {
-  // Ask user for the length of password
-  var promptLength = parseInt(window.prompt('What is a length of your password from 8 to 128 characters?'), 10);
+  let checks = {};
 
-  // Conditional Recursive Function Call
-  if (isNaN(promptLength) || promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
-    window.alert("You need to provide a valid answer! Please try again.");
-    return writePassword();
+  for(let i = 1; i < inputs.length; i++){
+    checks[inputs[i].name] = inputs[i].checked;
   }
 
-  while (true) {
-    // Ask user for the uppercase letters of password
-    var promptUpCase = window.confirm('Do you want uppercase letters?');
-    // Ask user for the lowercase letters of password
-    var promptLowCase = window.confirm('Do you want lowercase letters?');
-    // Ask user for the digits letters of password
-    var promptDigits = window.confirm('Do you want digits?');
-    // Ask user for the special symbols letters of password
-    var promptSpecials = window.confirm('Do you want special symbols?');
+  // while (true) {
+  //   // // Ask user for the uppercase letters of password
+  //   // var promptUpCase = window.confirm('Do you want uppercase letters?');
+  //   // // Ask user for the lowercase letters of password
+  //   // var promptLowCase = window.confirm('Do you want lowercase letters?');
+  //   // // Ask user for the digits letters of password
+  //   // var promptDigits = window.confirm('Do you want digits?');
+  //   // // Ask user for the special symbols letters of password
+  //   // var promptSpecials = window.confirm('Do you want special symbols?');
     
-    // Check if any of criteria was chosen
-    if (promptUpCase === true || promptLowCase === true || promptDigits === true || promptSpecials === true) {
-      break;
-    }
-    else {
-      window.alert('You need to choose at least one character type');
-    }
-  }
+  //   // Check if any of criteria was chosen
+  //   if (checks.uppercase === true || checks.lowercase === true || checks.digits === true || checks.symbols === true) {
+  //     break;
+  //   }
+  //   else {
+  //     window.alert('You need to choose at least one character type');
+  //   }
+  // }
 
-  var password = generatePassword(promptLength, promptUpCase, promptLowCase, promptDigits, promptSpecials);
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword(getLength.value, checks.uppercase, checks.lowercase, checks.digits, checks.symbols);
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
+}
+
+getLength.onchange = function(){
+  console.log(getLength.value);
+  if (isNaN(getLength.value) || getLength.value === null || getLength.value < 8 || getLength.value > 128) {
+    window.alert("You need to provide a valid answer! Please try again.");
+  }
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
